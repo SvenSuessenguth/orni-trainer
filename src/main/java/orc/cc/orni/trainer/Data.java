1f76c0132757d96dcea048e1aa9ec61bb1a20dce
U000349 <sven.suessenguth@signal-iduna.de>
2026-03-17 21:32:19 +0100

    Warnungen beseitigt

--- src/main/java/orc/cc/orni/trainer/Data.java
+++ src/main/java/orc/cc/orni/trainer/Data.java
@@ -8,7 +8,6 @@ import jakarta.json.bind.JsonbBuilder;
 
 import java.io.File;
 import java.io.FileReader;
-import java.io.FilenameFilter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,29 +16,40 @@ import java.util.Objects;
 
 @ApplicationScoped
 public class Data implements Serializable {
-    List<Bird> birds = new ArrayList<>();
+    private final List<Bird> birds = new ArrayList<>();
 
-    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) throws Exception {
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
         var classLoader = Thread.currentThread().getContextClassLoader();
         var birdsFolder = new File(Objects.requireNonNull(classLoader.getResource("/META-INF/resources/birds")).getPath());
+
         // In jedem subFolder ist eine Art hinterlegt
         var subFolders = birdsFolder.listFiles();
-
-        var fileNameFilter = new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return "meta-data.json".equals(name);
-            }
-        };
-        for (var subFolder : subFolders) {
-            var file = Arrays.stream(subFolder.listFiles(fileNameFilter)).findFirst().orElseThrow();
-            var reader = Json.createReader(new FileReader(file));
-            var jsonst = reader.read();
-
-            var bird = JsonbBuilder.create().fromJson(jsonst.toString(), Bird.class);
-            birds.add(bird);
+        if (subFolders == null) {
+            return;
         }
 
-        System.out.println(birds);
+        // Lesen der Metadaten in jedem SubFolder zu jeder Art
+        for (var subFolder : subFolders) {
+            // In jedem SubFolder musss es eine Datei mit Metadaten geben
+            var metaDataFiles = subFolder.listFiles((dir, name) -> "meta-data.json".equals(name));
+            if (metaDataFiles == null) {
+                continue;
+            }
+            var metaDataFile = Arrays.stream(metaDataFiles).findFirst().orElseThrow();
+
+            try (var jsonb = JsonbBuilder.create()) {
+                try (var metaDataReader = Json.createReader(new FileReader(metaDataFile))) {
+                    var jsonStructure = metaDataReader.read();
+                    var bird = jsonb.fromJson(jsonStructure.toString(), Bird.class);
+                    birds.add(bird);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public List<Bird> getBirds() {
+        return birds;
     }
 }
