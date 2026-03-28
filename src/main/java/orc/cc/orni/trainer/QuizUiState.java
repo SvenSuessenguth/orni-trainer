cb68ee50deda33d02aad94b441fa153f14a5aaba
SvenSuessenguth <familie.suessenguth@googlemail.com>
2026-03-28 12:24:31 +0100

    Korrekturen

--- src/main/java/orc/cc/orni/trainer/QuizUiState.java
+++ src/main/java/orc/cc/orni/trainer/QuizUiState.java
@@ -1,36 +1,41 @@
 package orc.cc.orni.trainer;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Data;
+import lombok.extern.java.Log;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @ViewScoped
 @Named
 @Data
+@Log
 public class QuizUiState implements Serializable {
-  @Inject
-  private Resources resources;
+    @Inject
+    private Resources resources;
 
-  private Category category;
-  private List<Bird> birdsInCategory;
-  private List<Bird> birdsAnsweredCorrectly;
-  private List<Bird> birdsAnsweredWrongly;
+    private Category category;
+    private List<Bird> birdsInCategory;
+    private List<Bird> birdsAnsweredCorrectly;
+    private List<Bird> birdsAnsweredWrongly;
 
-  public String getRandomBirdRandomImage() {
-    var countBirds = resources.getBirds().size();
-    var randomBirdNumber = ThreadLocalRandom.current().nextInt(0, countBirds);
-    var randomBird = resources.getBirds().get(randomBirdNumber);
+    public String getRandomBirdRandomImage() {
+        var countBirds = resources.getBirds().size();
+        var randomBirdNumber = ThreadLocalRandom.current().nextInt(0, countBirds);
+        log.info("Random Bird number is: " + randomBirdNumber);
+        var randomBird = resources.getBirds().get(randomBirdNumber);
 
-    var path = randomBird.getArt().replace(" ", "_");
-    var countImages = randomBird.getImages().size();
-    var randomImageNumber = ThreadLocalRandom.current().nextInt(0, countImages);
-    var randomImageName = randomBird.getImages().get(randomImageNumber).name();
+        var path = randomBird.getArt().replace(" ", "_");
+        var countImages = randomBird.getImages().size();
+        var randomImageNumber = ThreadLocalRandom.current().nextInt(0, countImages);
+        log.info("Random image number is: " + randomImageNumber);
+        var randomImageName = randomBird.getImages().get(randomImageNumber).name();
 
-    return path.toLowerCase() + "/" + randomImageName;
-  }
+        log.info("randomImageName: " + path.toLowerCase() + "/" + randomImageName);
+        return path.toLowerCase() + "/" + randomImageName;
+    }
 }
