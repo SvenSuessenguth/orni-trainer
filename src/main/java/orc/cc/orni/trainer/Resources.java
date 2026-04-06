96c741e3eae2fb55e02aea12a5f5ef8ce0705a87
SvenSuessenguth <familie.suessenguth@googlemail.com>
2026-04-06 14:16:24 +0200

    Meta-data korrigiert

--- src/main/java/orc/cc/orni/trainer/Resources.java
+++ src/main/java/orc/cc/orni/trainer/Resources.java
@@ -1,5 +1,11 @@
 package orc.cc.orni.trainer;
 
+import jakarta.faces.view.ViewScoped;
+import jakarta.json.Json;
+import jakarta.json.bind.JsonbBuilder;
+import lombok.Data;
+import lombok.extern.java.Log;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.Serializable;
@@ -8,46 +14,40 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-import jakarta.faces.view.ViewScoped;
-import jakarta.json.Json;
-import jakarta.json.bind.JsonbBuilder;
-import lombok.Data;
-import lombok.extern.java.Log;
-
 @ViewScoped
 @Data
 @Log
 public class Resources implements Serializable {
-  private final List<Bird> birds = new ArrayList<>();
+    private final List<Bird> birds = new ArrayList<>();
 
-  public Resources() {
-    var classLoader = Thread.currentThread().getContextClassLoader();
-    var birdsFolder = new File(Objects.requireNonNull(classLoader.getResource("/META-INF/resources/birds")).getPath());
+    public Resources() {
+        var classLoader = Thread.currentThread().getContextClassLoader();
+        var birdsFolder = new File(Objects.requireNonNull(classLoader.getResource("/META-INF/resources/birds")).getPath());
 
-    // In jedem subFolder ist eine Art hinterlegt
-    var subFolders = birdsFolder.listFiles();
-    if (subFolders == null) {
-      return;
-    }
-
-    // Lesen der Metadaten in jedem SubFolder zu jeder Art
-    for (var subFolder : subFolders) {
-      // In jedem SubFolder musss es eine Datei mit Metadaten geben
-      var metaDataFiles = subFolder.listFiles((dir, name) -> "meta-data.json".equals(name));
-      if (metaDataFiles == null) {
-        continue;
-      }
-      var metaDataFile = Arrays.stream(metaDataFiles).findFirst().orElseThrow();
-
-      try (var jsonb = JsonbBuilder.create()) {
-        try (var metaDataReader = Json.createReader(new FileReader(metaDataFile))) {
-          var jsonObject = metaDataReader.readObject();
-          var bird = jsonb.fromJson(jsonObject.toString(), Bird.class);
-          birds.add(bird);
+        // In jedem subFolder ist eine Art hinterlegt
+        var subFolders = birdsFolder.listFiles();
+        if (subFolders == null) {
+            return;
         }
-      } catch (Exception e) {
-        log.severe(e.getMessage());
-      }
+
+        // Lesen der Metadaten in jedem SubFolder zu jeder Art
+        for (var subFolder : subFolders) {
+            // In jedem SubFolder musss es eine Datei mit Metadaten geben
+            var metaDataFiles = subFolder.listFiles((dir, name) -> "meta-data.json".equals(name));
+            if (metaDataFiles == null) {
+                continue;
+            }
+            var metaDataFile = Arrays.stream(metaDataFiles).findFirst().orElseThrow();
+
+            try (var jsonb = JsonbBuilder.create()) {
+                try (var metaDataReader = Json.createReader(new FileReader(metaDataFile))) {
+                    var jsonObject = metaDataReader.readObject();
+                    var bird = jsonb.fromJson(jsonObject.toString(), Bird.class);
+                    birds.add(bird);
+                }
+            } catch (Exception e) {
+                log.severe("Error reading: " + subFolder.getName());
+            }
+        }
     }
-  }
 }
